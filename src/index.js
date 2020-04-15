68b407b62a2d330381b79b6eaca84db176e9e510
khan <khan.ngo@gmail.com>
2020-04-15 01:29:06 -0400

    Updates profession

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
 const DATA = {
-	name: "Hi, I'm Khanh",
-	profession: 'Java Full-Stack Web Developer',
-	linkedinURL: 'https://www.linkedin.com/in/khanh-ngo-8863193',
-	githubURL: 'https://github.com/khan-ngo',
-	twitterURL: 'https://twitter.com/khan_ngo',
-	instaURL: 'https://www.instagram.com/kqngo/',
-	myGoal: "My goal is to pivot my career to full time software development at an established company."
+  name: "Hi, I'm Khanh",
+  profession: "",
+  linkedinURL: "https://www.linkedin.com/in/khanh-ngo-8863193",
+  githubURL: "https://github.com/khan-ngo",
+  twitterURL: "https://twitter.com/khan_ngo",
+  instaURL: "https://www.instagram.com/kqngo/",
+  myGoal:
+    "My goal is to pivot my career to full time software development at an established company.",
 };
 
-ReactDOM.render(<App profileData={DATA} />, document.getElementById('root'));
+ReactDOM.render(<App profileData={DATA} />, document.getElementById("root"));
 
 serviceWorker.unregister();
